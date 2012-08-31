76f864ec6618ecc4e8135eedcc7ac3aa0fcb3f85
Rex Sheng <shengning@gmail.com>
2012-08-31 11:05:18 +0800

    fix zxing headers

--- iphone/scripts/build_zxingwdiget_ios_sdk_static_lib.sh
+++ iphone/scripts/build_zxingwdiget_ios_sdk_static_lib.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
-#
-# Copyright 2004-present Facebook. All Rights Reserved.
-#
 
-# This script will build a static library version of the Facebook iOS SDK.
-# You may want to use this script if you have a project that has the 
-# Automatic Reference Counting feature turned on. Once you run this script
-# you will get a directory under the iOS SDK project home path:
-#    lib/facebook-ios-sdk
-# You can drag the facebook-ios-sdk directory into your Xcode project and
-# copy the contents over or include it as a reference.
-
-# Function for handling errors
 die() {
     echo ""
     echo "$*" >&2
@@ -40,7 +28,6 @@ PROJECT_HOME=`pwd`
 
 echo "Project Home: $PROJECT_HOME"
 
-# The facebook-ios-sdk src directory path
 SRCPATH=$PROJECT_HOME
 
 # The directory where the target is built
@@ -73,6 +60,8 @@ lipo -create $BUILDDIR/Release-iphonesimulator/libZXingWidget.a $BUILDDIR/Releas
 
 echo "Step 5 : Copy headers Needed"
 \cp $SRCPATH/Classes/*.h $LIBOUTPUTDIR/
+mkdir -p $LIBOUTPUTDIR/parsedResults
+\cp $SRCPATH/Classes/parsedResults/ParsedResult.h $LIBOUTPUTDIR/parsedResults/
 
 echo "Finished Universal ZXingWidget Generation"
 echo ""
